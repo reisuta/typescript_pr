224386e97a25c8233395b73d3456e0fd1b78eb8f
amaki <in345vain@gmail.com>
2021-07-06 07:18:12 +0900

    I understand for

--- src/namespace.ts
+++ src/namespace.ts
@@ -9,11 +9,28 @@ export {};
 // const me = new aaa.Person('です');
 // console.log(me.name);
 
-class Animal {
- run():string {
-   return 'I can run';
- }
-}
+// class Animal {
+//  run():string {
+//    return 'I can run';
+//  }
+// }
 
-let animal = new Animal();
-console.log(animal.run());
+// let animal = new Animal();
+// console.log(animal.run());
+
+//genericsについて
+// const echo = <T> (arg:T):T =>{
+// return arg;
+// };
+
+// console.log(echo<number>(100));
+// console.log(echo<string>('helo'));
+
+// console.log(echo<boolean>(true))
+
+
+
+//型アサーション
+let name:any = "amane";
+let length = (name as string).length;
+console.log(length);
---
+++ src/partial.ts
@@ -0,0 +1,15 @@
+export {};
+
+let dd = "aaaa";
+// console.log(dd);
+let sum =0;
+for(let i=0; i<2; i++){
+  // for(let j=0; j<2; j++){
+  //   console.log(11)
+  // }
+sum += 11;
+sum += 22;
+
+}
+
+console.log(sum);
